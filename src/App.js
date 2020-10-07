2c5203d03a96bb5008106931111d1077950cb04f
Oscar Wales <oscarjwales@gmail.com>
2020-10-07 15:05:34 +0100

    Installed react simple maps, created initial topo.json file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Home from './components/Home/Home';
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import "./styles/css/styles.min.css";
 
+import geoUrl from "./data/topo.json";
+
 const App = () => (
-  <Home />
+  <>
+    <Home />
+    <div>
+      <ComposableMap width="1000">
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+          }
+        </Geographies>
+      </ComposableMap>
+    </div>
+  </>
 );
 
 export default App;
