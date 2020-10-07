44f5b00348ce5a52cfa32fbeeca1c6364a4aa12c
Oscar Wales <oscarjwales@gmail.com>
2020-10-07 16:52:24 +0100

    Added open sans font for tooltip font

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -2,16 +2,6 @@ import React, { memo } from 'react';
 import { ZoomableGroup, ComposableMap, Geographies, Geography } from "react-simple-maps";
 import geoUrl from "../../data/topo.json";
 
-const rounded = num => {
-    if (num > 1000000000) {
-      return Math.round(num / 100000000) / 10 + "Bn";
-    } else if (num > 1000000) {
-      return Math.round(num / 100000) / 10 + "M";
-    } else {
-      return Math.round(num / 100) / 10 + "K";
-    }
-};
-
 const Map = ({ setTooltipContent }) => (
     <div>
         <ComposableMap width="1200" data-tip="" projectionConfig={{ scale: 200 }}>
@@ -23,17 +13,25 @@ const Map = ({ setTooltipContent }) => (
                                 key={geo.rsmKey} 
                                 geography={geo}
                                 onMouseEnter={() => {
-                                    const { NAME, POP_EST } = geo.properties;
-                                    setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
+                                    const { NAME } = geo.properties;
+                                    setTooltipContent(`${NAME}`);
                                 }}
                                 onMouseLeave={() => {
                                     setTooltipContent("");
                                 }}
                                 fill="#44BBA4"
                                 stroke="#F6F7EB"
+                                strokeWidth="0.5"
                                 style={{
+                                    default: {
+                                        outline: 'none'
+                                    },
                                     hover: {
-                                    fill: "#E94F37"
+                                        fill: "#E94F37",
+                                        outline: 'none'
+                                    },
+                                    pressed: {
+                                        outline: 'none'
                                     }
                                 }} 
                             />
