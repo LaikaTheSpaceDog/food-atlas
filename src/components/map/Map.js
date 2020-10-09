b100fa772983c05dfbc5d24765242c29d23c09f4
Oscar Wales <oscarjwales@gmail.com>
2020-10-09 16:09:20 +0100

    Moved map and country together into single component, may no longer need Redux. Began experimenting with transitions for change between map and country view - not working yet

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,7 +1,9 @@
 import React, { memo, Component } from 'react';
 import { ZoomableGroup, ComposableMap, Geographies, Geography } from "react-simple-maps";
 import geoUrl from "../../data/topo.json";
-import { Link } from"react-router-dom";
+// import { Link } from"react-router-dom";
+import Country from '../Country/'
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class Map extends Component { 
 
@@ -12,10 +14,11 @@ class Map extends Component {
             country: "",
             dish: "",
             description: "",
-            photo: ""
+            photo: "",
+            selected: false
         }
 
-        this.handleClick = this.handleClick.bind(this);
+        // this.handleClick = this.handleClick.bind(this);
     }
 
     handleEnter(country, dish, description, photo){
@@ -27,55 +30,68 @@ class Map extends Component {
         })
     }
     
-    handleClick(){
-        this.props.handleClick({...this.state});
-    }
+    // handleClick(){
+    //     this.props.handleClick({...this.state});
+    // }
     
     render(){ 
     
+        const { country, dish, description, photo } = this.state;
+
         return(
-            <div className="container">
-                <ComposableMap width="1200" data-tip="" projectionConfig={{ scale: 200 }}>
-                    <ZoomableGroup>
-                        <Geographies geography={geoUrl}>
-                            {({ geographies }) =>
-                                geographies.map(geo =>
-                                    <Link to={ `/${geo.properties.NAME}` }> 
-                                        <Geography 
-                                            key={geo.rsmKey} 
-                                            geography={geo}
-                                            onMouseEnter={() => {
-                                                const { NAME, DISH, DESCRIPTION, PHOTO } = geo.properties;
-                                                this.props.setTooltipContent(`${NAME}`);
-                                                this.handleEnter(NAME, DISH, DESCRIPTION, PHOTO);
-                                            }}
-                                            onMouseLeave={() => {
-                                                this.props.setTooltipContent("");
-                                            }}
-                                            fill="#44BBA4"
-                                            stroke="#F6F7EB"
-                                            strokeWidth="0.5"
-                                            style={{
-                                                default: {
-                                                    outline: 'none'
-                                                },
-                                                hover: {
-                                                    fill: "#E94F37",
-                                                    outline: 'none'
-                                                },
-                                                pressed: {
-                                                    outline: 'none'
-                                                }
-                                            }}
-                                            onClick={ this.handleClick } 
-                                        />
-                                    </Link>
-                                )
-                            }
-                        </Geographies>
-                    </ZoomableGroup>
-                </ComposableMap>
-            </div>
+            <>
+                { !this.state.selected ?
+                    <div className="container">
+                        <ComposableMap width="1200" data-tip="" projectionConfig={{ scale: 200 }}>
+                            <ZoomableGroup>
+                                <Geographies geography={geoUrl}>
+                                    {({ geographies }) =>
+                                        geographies.map(geo =>
+                                            // <Link to={ `/${geo.properties.NAME}` }> 
+                                                <Geography 
+                                                    key={geo.rsmKey} 
+                                                    geography={geo}
+                                                    onMouseEnter={() => {
+                                                        const { NAME, DISH, DESCRIPTION, PHOTO } = geo.properties;
+                                                        this.props.setTooltipContent(`${NAME}`);
+                                                        this.handleEnter(NAME, DISH, DESCRIPTION, PHOTO);
+                                                    }}
+                                                    onMouseLeave={() => {
+                                                        this.props.setTooltipContent("");
+                                                    }}
+                                                    fill="#44BBA4"
+                                                    stroke="#E94F37"
+                                                    strokeWidth="0.5"
+                                                    style={{
+                                                        default: {
+                                                            outline: 'none'
+                                                        },
+                                                        hover: {
+                                                            fill: "#E94F37",
+                                                            outline: 'none'
+                                                        },
+                                                        pressed: {
+                                                            outline: 'none'
+                                                        }
+                                                    }}
+                                                    onClick={() => { 
+                                                        // this.handleClick
+                                                        this.setState({
+                                                            selected: true
+                                                        });
+                                                        this.props.setTooltipContent("");
+                                                    }} 
+                                                />
+                                            // </Link>
+                                        )
+                                    }
+                                </Geographies>
+                            </ZoomableGroup>
+                        </ComposableMap>
+                    </div>
+                : 
+                <Country country={ country } dish={ dish } description={ description } photo={ photo } /> }
+            </>
         );
     }
 }
