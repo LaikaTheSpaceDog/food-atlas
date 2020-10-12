c6c2dd8002ac41430604fef1c40412002beea267
Oscar Wales <oscarjwales@gmail.com>
2020-10-12 17:18:45 +0100

    'about' and 'list' sections now appear when buttons in header are clicked, added some styling. Need to continue with styling, add transitions so they don't just suddenly appear

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -2,7 +2,7 @@ import React, { memo, Component } from 'react';
 import { ZoomableGroup, ComposableMap, Geographies, Geography } from "react-simple-maps";
 import geoUrl from "../../data/topo.json";
 import Country from '../Country/'
-import { TransitionGroup, CSSTransition, SwitchTransition } from "react-transition-group";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 class Map extends Component { 
 
@@ -14,10 +14,14 @@ class Map extends Component {
             dish: "",
             description: "",
             photo: "",
-            selected: false
+            selected: false,
+            about: false,
+            list: false
         }
 
         this.handleBack = this.handleBack.bind(this);
+        this.handleAbout = this.handleAbout.bind(this);
+        this.handleList = this.handleList.bind(this);
     }
 
     handleEnter(country, dish, description, photo){
@@ -38,10 +42,24 @@ class Map extends Component {
             selected: false
         })
     }
+
+    handleAbout(){
+        let current = this.state.about;
+        this.setState({
+            about: !current
+        })
+    }
+
+    handleList(){
+        let current = this.state.list;
+        this.setState({
+            list: !current
+        })
+    }
     
     render(){ 
     
-        const { country, dish, description, photo, selected } = this.state;
+        const { country, dish, description, photo, selected, about, list } = this.state;
 
         return(
             <SwitchTransition>
@@ -57,9 +75,9 @@ class Map extends Component {
                         <> 
                             <section className="map">
                                 <header className="header">
-                                    <h2 className="subHeading">About</h2>
+                                    <button className="headButton" onClick={ this.handleAbout }><h2 className="subHeading">About</h2></button>
                                     <h1 className="heading">Food Atlas</h1>
-                                    <h2 className="subHeading">List</h2>
+                                    <button className="headButton" onClick={ this.handleList }><h2 className="subHeading">List</h2></button>
                                 </header>
                                 <div className="container">
                                     <ComposableMap width="1200" data-tip="" projectionConfig={{ scale: 200 }} >
@@ -106,13 +124,11 @@ class Map extends Component {
                                         </ZoomableGroup>
                                     </ComposableMap>
                                 </div>
-                                <aside className="about">
-                                    <h2 className="subHeading">About</h2>
-                                    <p className="text">Welcome to the Food Atlas! Here you can travel around the world in 197 dishes by simply clicking on a country to find out about one of its signature national dishes.</p>
-                                    <p className="text">Some small nations may be hard to locate on the map due to its resolution, so please find them on the list instead if you are struggling!</p>
+                                <aside className="about" style={{ display: about ? "block" : "none" }}>
+                                    <p className="asideText">Welcome to the Food Atlas! Here you can travel around the world in 197 dishes by simply clicking on a country to find out about one of its signature national dishes.</p>
+                                    <p className="asideText">Some small nations may be hard to locate on the map due to its resolution, so please find them on the list instead if you are struggling!</p>
                                 </aside>
-                                <aside className="list">
-                                    <h2 className="subHeading">Countries</h2>
+                                <aside className="list" style={{ display: list ? "block" : "none" }}>
                                     <ul>
 
                                     </ul>
