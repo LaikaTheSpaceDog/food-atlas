665fd2772c2be4a54bb9c6f27180069113032836
Oscar Wales <oscarjwales@gmail.com>
2020-10-19 17:08:07 +0100

    Added some media queries for headings, did some styling of asides for scroll feature

--- src/components/Map.js
+++ src/components/Map.js
@@ -138,9 +138,11 @@ class Map extends Component {
                             <div className="overlay" id="about">
                                 <aside className="about">
                                     <a class="close" href="#" onClick={ this.handleAbout }>&times;</a>
-                                    <p className="asideText centre">Welcome to the Food Atlas!</p>
-                                    <p className="asideText">Travel around the world in 197(ish) dishes by simply clicking on a country to find out about one of its signature national dishes.</p>
-                                    <p className="asideText">Some small nations may be hard to locate on the map due to its resolution, so please find them on the list instead if you are struggling!</p>
+                                    <div className="aboutText">
+                                        <p className="asideText centre">Welcome to the Food Atlas!</p>
+                                        <p className="asideText">Travel around the world in 197(ish) dishes by simply clicking on a country to find out about one of its signature national dishes.</p>
+                                        <p className="asideText">Some small nations may be hard to locate on the map due to its resolution, so please find them on the list instead if you are struggling!</p>
+                                    </div>
                                 </aside>
                             </div>
                             <div className="overlay" id="list">
