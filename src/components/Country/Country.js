a48eeada7eb3f0950d75e24c6bee7cfba8dfb01b
Oscar Wales <oscarjwales@gmail.com>
2020-10-19 14:46:47 +0100

    Added explainer message for countries that haven't had their data added yet

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -4,6 +4,20 @@ import back from "../../styles/assets/svg/angle-circle-left.svg";
 const Country = ({ country, dish, description, photo, handleBack }) => {
 
     return (
+        !dish ? 
+        <article className="country">
+            <header className="countryHeader">
+                <a href="#" onClick={ handleBack }><img alt="backwards arrow" src={ back } className="button"></img></a>
+                <div className="countryTitles">
+                    <h1 className="heading">{ country }</h1>
+                    <h2 className="subHeading">It's a mystery to me to...</h2>
+                </div>
+            </header>
+            <section className="countryBody">
+                <p className="text description">The data for this country hasn't been added yet - please check back later!</p>
+            </section>
+        </article>
+        :
         <article className="country">
             <header className="countryHeader">
                 <a href="#" onClick={ handleBack }><img alt="backwards arrow" src={ back } className="button"></img></a>
