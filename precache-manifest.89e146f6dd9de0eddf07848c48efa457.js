f79cf76653a19314ccccffa4ac038d711c0aea1c
Oscar Wales <oscarjwales@gmail.com>
2020-10-20 10:47:04 +0100

    Updates

---
+++ precache-manifest.89e146f6dd9de0eddf07848c48efa457.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99718c8666b2e4b9d5b54cd6f19ff907",
+    "url": "/food-atlas/index.html"
+  },
+  {
+    "revision": "440788a484079862305d",
+    "url": "/food-atlas/static/css/main.1503dc41.chunk.css"
+  },
+  {
+    "revision": "3a84453939e4810aed6f",
+    "url": "/food-atlas/static/js/2.0e92558e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-atlas/static/js/2.0e92558e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "440788a484079862305d",
+    "url": "/food-atlas/static/js/main.b95ec144.chunk.js"
+  },
+  {
+    "revision": "baca7c4d3e2daed2abbc",
+    "url": "/food-atlas/static/js/runtime-main.d4478573.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/food-atlas/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "42008c4b6f373eb30ab706371f699efc",
+    "url": "/food-atlas/static/media/Sniglet Regular.42008c4b.otf"
+  },
+  {
+    "revision": "7e4b174eefa08543a0c22a55bfb4ff05",
+    "url": "/food-atlas/static/media/Sniglet Regular.7e4b174e.ttf"
+  },
+  {
+    "revision": "01f72c9e186f033e3cf418d47f1d2d95",
+    "url": "/food-atlas/static/media/angle-circle-left.01f72c9e.svg"
+  }
+]);
