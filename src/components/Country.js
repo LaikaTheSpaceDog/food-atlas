50b96fbae66122fc5b76a15f0c3afd113c397c5a
Oscar Wales <oscarjwales@gmail.com>
2020-10-23 15:03:33 +0100

    Made links orange on hover, switched positions of close button and recipe link on country component

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import back from "../styles/assets/svg/angle-circle-left.svg";
+import close from "../styles/assets/svg/close-round-line.svg";
 import recipeIcon from "../styles/assets/svg/recipe-book.svg";
 
 const Country = ({ country, dish, description, photo, recipe, handleBack, handlePhotoSource }) => {
@@ -8,11 +8,11 @@ const Country = ({ country, dish, description, photo, recipe, handleBack, handle
         !dish ? 
         <article className="country">
             <header className="countryHeader">
-                <a href="#home" onClick={ handleBack }><img alt="backwards arrow" src={ back } className="button"></img></a>
                 <div className="countryTitles">
                     <h1 className="heading">{ country }</h1>
                     <h2 className="subHeading">It's a mystery to me to...</h2>
                 </div>
+                <a href="#home" onClick={ handleBack }><img alt="close symbol" src={ close } className="button"></img></a>
             </header>
             <section className="countryBody">
                 <p className="text description">The data for this country hasn't been added yet - please check back later!</p>
@@ -21,12 +21,12 @@ const Country = ({ country, dish, description, photo, recipe, handleBack, handle
         :
         <article className="country">
             <header className="countryHeader">
-                <a href="#home" onClick={ handleBack }><img alt="backwards arrow" src={ back } className="button"></img></a>
+                <a href={ recipe } target="_blank" rel="noopener noreferrer"><img alt="recipe book icon" src={ recipeIcon } className="link"></img></a>
                 <div className="countryTitles">
                     <h1 className="heading">{ country }</h1>
                     <h2 className="subHeading">{ dish }</h2>
                 </div>
-                <a href={ recipe } target="_blank" rel="noopener noreferrer"><img alt="recipe book icon" src={ recipeIcon } className="link"></img></a>
+                <a href="#home" onClick={ handleBack }><img alt="backwards arrow" src={ close } className="button"></img></a>
             </header>
             <section className="countryBody">
                 <figure className="photoContainer">
