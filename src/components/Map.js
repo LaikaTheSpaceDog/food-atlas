29b75b8c1bec17e300158925dcc03aff1c454a53
Oscar Wales <oscarjwales@gmail.com>
2020-10-26 14:31:50 +0000

    Edited close and recipe buttons on country component so they change colour on hover

--- src/components/Map.js
+++ src/components/Map.js
@@ -142,7 +142,7 @@ class Map extends Component {
                                     <div className="aboutText">
                                         <p className="asideText centre">Welcome to the Food Atlas!</p>
                                         <p className="asideText">Travel around the world in 197(ish) dishes by simply clicking on a country to find out about one of its signature national dishes.</p>
-                                        <p className="asideText">Some small nations may be hard to locate on the map due to its resolution, so please find them on the list instead if you are struggling!</p>
+                                        <p className="asideText">Some small nations may be hard to locate on the map due to its resolution, so please find them on the <a className="link" href="#list">list</a> instead if you are struggling!</p>
                                     </div>
                                 </aside>
                             </div>
