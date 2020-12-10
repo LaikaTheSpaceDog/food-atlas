a78ff9e5bf601515bb24d7267ad105397e81f63e
Oscar Wales <oscarjwales@gmail.com>
2020-12-10 11:18:52 +0000

    Updated index.js file with Provider and store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
-    <App />,
+  <Provider store={ store }>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
