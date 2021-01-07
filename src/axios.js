971aaba76fe7f0ca7c121337c6910fe05622ba2f
Oscar Wales <oscarjwales@gmail.com>
2021-01-07 17:48:57 +0000

    Removed axios-delay

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-import delayAdapterEnhancer from 'axios-delay';
 
 export default axios.create({
     baseURL: "https://food-atlas.laikathespacedog.co.uk/api",
     headers: {
         Accept: "application/json"
-    },
-    adapter: delayAdapterEnhancer(axios.defaults.adapter)
+    }
 });
